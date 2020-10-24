1b9fcf4cf8bdbb46a3c1db296e61188e04a14160
jqha <jqha@umd.edu>
2020-10-24 15:42:21 -0400

    Lab 6 (Finished)
    Completed Lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,4 @@
 // You may wish to find an effective randomizer function on MDN.
-
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -17,6 +16,17 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+//Durstenfeld Shuffle takes in array and returns shuffled array
+function durstenfeldShuffle(array) {
+  for(let i = array.length - 1; i > 0; i--){
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i]
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -29,8 +39,47 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+
+      //Shuffles list of countries
+      const shuffled = durstenfeldShuffle(fromServer);
+      //Grabs first 10 countries from shuffled list
+      const tenCountries = shuffled.slice(0, 10);
+      //Sorts array of 10 countries by reverse alphabetical order
+      const reverseSorted = tenCountries.sort((a, b) => sortFunction(b, a, 'name'));
+
+      $('.flex-inner').remove();
+      //Creates new ordered list
+      const ol = document.createElement('ol');
+      //Sets ordered list to class name flex-inner
+      ol.className = 'flex-inner';
+
+      //Inserts ordered list as first child element to form
+      const parentForm = document.querySelector('form');
+      parentForm.prepend(ol);
+
+      //For loop appending checkbox items to ordered list
+      for(let i = 0; i < reverseSorted.length; i++) {
+        //Creates list element
+        const li = document.createElement('li');
+
+        //Creates input attribute and sets its type, value, and id
+        const input = document.createElement('input');
+          input.type = 'checkbox';
+          input.value = reverseSorted[i].code;
+          input.id = reverseSorted[i].code;
+        //Appends input attribute to list element
+        li.appendChild(input);
+
+        //Creates label attribute and sets what its for and text
+        const label = document.createElement('label');
+          label.setAttribute('for', reverseSorted[i].code);
+          label.textContent = reverseSorted[i].name;
+        //Appends label to list element
+        li.appendChild(label);
+
+        //Appends list element to ordered list
+        ol.append(li);
+      }
     })
     .catch((err) => console.log(err));
 });
